d2965ca9aa4855832aebe4e8e74bb21502891052
Meshack <meshack.odunowe@decagon.dev>
2023-12-17 23:07:06 +0100

    metadata issues

--- app/layout.js
+++ app/layout.js
@@ -4,12 +4,14 @@ import NavBar from "./components/navbar/page";
 import Footer from "./footer/page";
 import { ToastContainer } from "react-toastify";
 import Whatsapp from "./components/Whatsapp/Whatsapp";
+import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Mozisha",
+ 
   description:
-    "Mozisha, a welcoming home for talents to achieve their full potential",
+    "Unlocking African Talents for Global Success. Welcome to Mozisha, Your Gateway to Premium Talents to Supercharge Business Growth.",
 };
 
 export default function RootLayout({ children }) {
@@ -19,6 +21,12 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+       <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="google-site-verification" content="Lp0VRsvcy7dFnbasOW4pGobR46Xvy1MA-T7cL2eCbGU" />
+        <link rel='icon' href='/favicon.png'/>
+      </Head>
       <ToastContainer autoClose={2000} />
       <head>
         <link rel='icon' href='/favicon.png'/>
