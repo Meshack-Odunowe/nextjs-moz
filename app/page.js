459bfff0cbccfde7ffdce6ce08e30b9c543c7a86
Meshack <meshack.odunowe@decagon.dev>
2024-01-14 10:00:39 +0100

    made some new updates

--- app/page.js
+++ app/page.js
@@ -77,7 +77,7 @@ function Home() {
               </div>
               <div>
                 <button className="bg-purple-700 text-white rounded-md md:px-4 md:py-2 py-2 hover:bg-purple-100 mb-4 hover:text-purple-800 hover:font-bold w-full">
-                  <Link href="/talents"onClick={handleGetStartedClick}>For Talents
+                  <Link href="/talents" onClick={handleGetStartedClick}>For Talents
                   </Link>
                 </button>
                 <p className="text-sm text-center mb-16">
@@ -86,7 +86,7 @@ function Home() {
               </div>
               <div>
                 <button className="bg-purple-700 text-white rounded-md md:px-4 md:py-2 py-2 hover:bg-purple-100 mb-4 hover:text-purple-800 hover:font-bold md:w-full justify-center items-center w-full">
-                  <Link href="/mentors"onClick={handleGetStartedClick}>For Mentors
+                  <Link href="/mentors" onClick={handleGetStartedClick}>For Mentors
                   </Link>
                 </button>
                 <p className="text-sm text-center mb-16">
