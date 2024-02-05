11bb052d3a6b5cb97b1487b657e134761b377c28
Meshack <meshack.odunowe@decagon.dev>
2024-02-05 11:33:09 +0100

    still on the metadata issue

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -3,7 +3,8 @@ import { client, urlFor } from "../../lib/sanity";
 import { PortableText } from '@portabletext/react'
 import readingTime from "reading-time";
 import Head from 'next/head';
-export const revalidate=10
+
+export const revalidate = 10;
 
 async function getData(slug) {
   const query = `*[_type == 'blog' && slug.current == '${slug}']{
@@ -17,22 +18,23 @@ async function getData(slug) {
   const data = await client.fetch(query);
   return data;
 }
+
 function extractTextFromPortableText(content) {
   return content
     .map((block) => {
-      if (block.children) {
-        return block.children.map((child) => child.text).join(" ");
-      }
-      return block.text;
-    })
-    .join(" ");
+    if (block.children) {
+      return block.children.map((child) => child.text).join(" ");
+    }
+    return block.text;
+  })
+  .join(" ");
 }
+
 export async function generateMetadata({ params }) {
   const blog = await getData(params.slug);
   const title = blog.title;
   const description = blog.smallDescription;
-  const imageURL = urlFor(blog.titleImage).url();
-
+  const imageURL = urlFor(blog.titleImage).width(1200).height(630).url(); // Set width and height
   return {
     title: title,
     description: description,
@@ -40,30 +42,40 @@ export async function generateMetadata({ params }) {
       images: [
         {
           url: imageURL,
-          
+          width: 1200, // Optional: Provide the actual width of the image
+          height: 630, // Optional: Provide the actual height of the image
         },
       ],
     },
   };
 }
 
-
 export default async function BlogArticle({ params }) {
   const data = await getData(params.slug);
   const textForReadingTime = extractTextFromPortableText(data.content);
 
+  function truncateDescription(description, maxLength) {
+    if (description.length > maxLength) {
+      return description.substring(0, maxLength) + "...";
+    }
+    return description;
+  }
+
+  // Invoke the truncateDescription function with the actual parameters
+  const truncatedDescription = truncateDescription(data.smallDescription, 200);
+
   return (
     <>
       <Head>
-  <title>{data.title}</title>
-  <meta name="description" content={data.smallDescription} />
+        <title>{data.title}</title>
+        <meta name="description" content={truncatedDescription} />
 
-  <meta property="og:title" content={data.title} />
-  <meta property="og:description" content={data.smallDescription} />
-  <meta property="og:image" content={urlFor(data.titleImage).url()} />
-  <meta property="og:url" content={`https://www.mozisha.com/blog/${data.currentSlug}`} />
-  <meta property="og:type" content="article" />
-</Head>
+        <meta property="og:title" content={data.title} />
+        <meta property="og:description" content={truncatedDescription} />
+        <meta property="og:image" content={urlFor(data.titleImage).url()} />
+        <meta property="og:url" content={`https://mozisha.com/blog/${data.currentSlug}`} />
+        <meta property="og:type" content="article" />
+      </Head>
 
      
     <h1 className=" text-3xl block  font-bold leading-8 tracking-tight sm:text-4xl text-center my-8">
