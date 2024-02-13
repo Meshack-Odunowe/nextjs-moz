a1eeb2ba732d18eefc158eb6a5adbbda8ccb844b
Meshack <meshack.odunowe@decagon.dev>
2024-02-13 16:47:19 +0100

    width for blogpost fixed

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -103,7 +103,7 @@ export default async function BlogArticle({ params }) {
         priority
         className="rounded-md border  mt-8  mx-auto object-cover"
       />{" "}
-      <div className=" mt-16  mx-auto prose prose-lg prose-a:underline prose-blue px-4   sm:max-w-none  leading-8 mb-16 max-w-4xl">
+      <div className=" mt-16  mx-auto prose prose-lg prose-a:underline prose-blue px-4     leading-8 mb-16 max-w-4xl">
         <PortableText
           value={data.content}
           className="px-4 prose-blue max-w-4xl"
